3ab6f7fcbfca945264022ccfd4fd208026789897
aaron (kb) <aaron.sanders342@gmail.com>
2022-06-03 22:14:04 -0700

    wip: setting up backend, set up postgresql db, init react task component

---
+++ to-do-time/src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const tasks = useState([]);
+
+  useEffect(() => {
+    
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">To-Do List</header>
+
+    </div>
+  );
+}
+
+export default App;
