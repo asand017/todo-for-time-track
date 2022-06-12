c4daeafbcb0834d7f5af01a9c9ffe69895ea5ad5
aaron (kb) <aaron.sanders342@gmail.com>
2022-06-11 18:59:30 -0700

    point frontend api calls to localhost

--- todo-time/src/api/apiService.js
+++ todo-time/src/api/apiService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getTodos = () => {
-    return axios.get('http://192.168.1.34:3001/todos/fetchTodos').then((data) => {
+    return axios.get('http://localhost:3001/todos/fetchTodos').then((data) => {
         console.log(data);
         return data;
     })
@@ -9,7 +9,7 @@ export const getTodos = () => {
 
 export const addTodo = async (parms) => {
     console.log(parms);
-    const { response } = await axios.post(`http://192.168.1.34:3001/todos/addTodo`, {}, {headers: {
+    const { response } = await axios.post(`http://localhost:3001/todos/addTodo`, {}, {headers: {
             'Content-Type': 'application/json',
             'Access-Control-Allow-Origin': '*'
         }, params: parms,
