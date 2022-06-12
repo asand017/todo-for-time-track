657dd13ae2cb2feef339c67c55d8d8d6e7aac6f8
aaron (kb) <aaron.sanders342@gmail.com>
2022-06-11 18:49:29 -0700

    changed project heierachy for heroku deploy(github)

---
+++ routes/todos.js
@@ -0,0 +1,28 @@
+const Router = require('express-promise-router')
+const router = new Router()
+const db = require('../db')
+
+module.exports = router
+
+/*router.get('/', async (req, res) => {
+    //console.log("request from client:", req)
+    const { rows } = await db.query('SELECT NOW()')
+    console.log(rows)
+    res.send(rows)
+})*/
+
+router.get('/fetchTodos', async (req, res) => {
+    //console.log(req.query)
+    const { rows } = await db.query('SELECT * FROM tasks')
+    console.log("fetching rows: ", rows)
+    res.send(rows)
+})
+
+router.post('/addTodo', async (req, res) => {
+    const text = 'INSERT INTO tasks(name, description, priority, start_time, end_time) VALUES ($1, $2, $3, $4, $5)'
+    console.log("create todo: ", req.query)
+    const values = Object.keys(req.query).map((key) => req.query[key])
+    console.log(values)
+    const { rows } = await db.query(text, values)
+    res.send(rows[0])
+})
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mountRoutes = require('./routes')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
+const app = express()
+const port = 3001
+
+app.use(bodyParser.json())
+
+app.use(cors({
+    origin: "*"//['http://localhost:3000', '192.168.1.4']
+}))
+
+mountRoutes(app)
+
+app.use('/', express.static(path.resolve(__dirname, "./todo-time/build")))
+
+app.listen(port, () => {
+    console.log('server running on port', port)
+})
