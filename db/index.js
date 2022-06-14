9020780466548000c9f0ac9adc159eb1a99f2739
aaron (kb) <aaron.sanders342@gmail.com>
2022-06-14 10:15:51 -0700

    database_url env variable not working, trying hardcoded

--- db/index.js
+++ db/index.js
@@ -1,6 +1,7 @@
 const { Pool } = require('pg')
 require('dotenv').config();
 
+//console.log(process.env.NODE_ENV, process.env.DATABASE_URL)
 const pool = process.env.NODE_ENV === 'development' ? new Pool({
     host: process.env.PG_HOST,
     port: process.env.PG_PORT,
@@ -8,9 +9,20 @@ const pool = process.env.NODE_ENV === 'development' ? new Pool({
     password: process.env.PG_PASSWORD,
     database: process.env.PG_DATABASE,
 }) : new Pool ({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
+    connectionString: "postgres://fglveyxtdxllvj:956654dd6a1962ae52e0db374f383f1fdd67a8413cf1c3964a0cb2e1604eff97@ec2-52-204-195-41.compute-1.amazonaws.com:5432/df6lru8d1qp9qm",
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
+
+/*
+const pool = new Pool ({
+    connectionString: "postgres://fglveyxtdxllvj:956654dd6a1962ae52e0db374f383f1fdd67a8413cf1c3964a0cb2e1604eff97@ec2-52-204-195-41.compute-1.amazonaws.com:5432/df6lru8d1qp9qm",
+    ssl: {
+        rejectUnauthorized: false
+    }
+})
+*/
 
 pool.on('error', (err, client) => {
     console.log('Unexpected error on idle client', err)
